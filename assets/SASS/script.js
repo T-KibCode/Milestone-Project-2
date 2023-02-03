782e53506c79b4f4d790fe7ddb43acc68f2ef13f
T-KibCode <tkibirige94@gmail.com>
2023-02-03 17:21:38 +0000

    empty commit to save progress

---
+++ assets/SASS/script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelector("[data-carousel-button")
+
+/* event lisenter used for swapping to the next imate*/
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carousel1button === "next ? 1 : -1"
+        const slides = button
+        .closest("[data-carousel]")
+        .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1 ;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true 
+    delete activeSlide.dataset.active
+    })
+})
