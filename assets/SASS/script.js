294534f1c4149c1cc26f755a08e8d2ed968bcbb9
T-KibCode <tkibirige94@gmail.com>
2023-02-22 23:14:42 +0000

    implemented a Timer and Health bar system in full
    Also performed slight refactors/reduction in
    certain areas of the code to aim to reduce amount
    of code on the page.
    
    Will be working to reduce indentation to clean the look
    of the code in JS, and move inline CSS attributes into a
    seperate CSS page in order to keep to clean coding standards outlined
    for HTML5 and CSS3 standards.

--- assets/SASS/script.js
+++ assets/SASS/script.js
@@ -25,6 +25,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -131,7 +132,34 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
+function determineWinner ({player, enemy}) {
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'The Match Is A Draw'
+        } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+        } else if (player.health < enemy.health) 
+            document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+        
+}
+
 //* function that tracks *// 
+let timer = 60
+function decreaseTimer () {
+    setTimeout(decreaseTimer, 1000)
+    if (timer > 0 ) {
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }   
+
+    if (timer === 0 ) {
+        determineWinner({player, enemy})
+    }
+}
+
+
+
+decreaseTimer()
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -168,7 +196,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false
-        document.querySelector('#enemyHealth').style.width = '20%'
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if(
@@ -179,7 +208,13 @@ function animate() {
         enemy.isAttacking
     ) {
         enemy.isAttacking = false
-        console.log('enemy attack successful')
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    //end game based on health //
+    if(enemy.health <= 0 || player.health <= 0 ) {
+        determineWinner({player, enemy})
     }
 }
 
