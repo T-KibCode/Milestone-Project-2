6056880cf3faf64137e2bff04259c3d8a37ffd99
T-KibCode <tkibirige94@gmail.com>
2023-02-24 01:59:21 +0000

    Refactoring JS code for load order within the console.
    Also created a background image within the canvas
    using the sprite class.

---
+++ assets/JS/classes.js
@@ -0,0 +1,78 @@
+class Sprite {
+    constructor({position, imageSrc }) {
+        this.position = position
+        this.width = 50
+        this.height = 150
+        //* creates image within javascript property using native API *//
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+
+    update() {
+        this.draw()
+    }
+
+}
+
+class Fighter {
+    constructor({position, velocity, color = 'red', offset }) {
+        this.position = position
+        this.velocity = velocity
+        this.width = 50
+        this.height = 150
+        this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
+        this.isAttacking
+        this.health = 100
+    }
+
+    draw() {
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //* This is where Attack box is drawn *//
+
+        if (this.isAttacking) {
+            c.fillStyle = 'green',
+            c.fillRect(
+                this.attackBox.position.x,
+                this.attackBox.position.y,
+                this.attackBox.position.width, 
+                this.attackBox.position.height,
+            )
+        }
+    }
+
+    update() {
+        this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y 
+        
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y 
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
+    }
+
+    attack() {
+        this.isAttacking = true
+        setTimeout(() => {
+           this.isAttacking = false
+        }, 100)
+    }
+}
