33f60be562cc09daf40c5788492114b052f75c5e
T-KibCode <110776212+T-KibCode@users.noreply.github.com>
2023-02-26 14:24:26 +0000

    Final changes in code to commited JS canvas game f
    final commits in file for the game function.

--- assets/JS/classes.js
+++ assets/JS/classes.js
@@ -133,24 +133,11 @@ class Fighter extends Sprite{
         this.isAttacking = true
     }
 
-    takehit() {
-        this.switchSprite('takeHit')
-        this.health -= 20
-    }
-
     switchSprite(sprite) {
-        //overring all other animations with the attack animation // 
         if (
             this.image === this.sprites.attack1.image &&
             this.framesCurrent < this.sprites.attack1.framesMax -1 
         ) 
-            return
-
-            // override when fighter gets hit //
-        if (
-            this.image === this.sprites.takeHit.image && 
-            this.framesCurrent < this.sprites.takeHit.framesMax -1
-        )
             return
         
         switch (sprite) {
@@ -186,13 +173,6 @@ class Fighter extends Sprite{
                 if (this.image !== this.sprites.attack1.image) {
                 this.image = this.sprites.attack1.image
                 this.framesMax = this.sprites.attack1.framesMax
-                this.framesCurrent = 0
-            }
-            break
-            case 'takeHit':
-                if (this.image !== this.sprites.takeHit.image) {
-                this.image = this.sprites.takeHit.image
-                this.framesMax = this.sprites.takeHit.framesMax
                 this.framesCurrent = 0
             }
         }
